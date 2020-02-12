b7365de14791a3e8868a75b291b11c0f6dc684d6
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-02-12 17:32:05 +0100

    primer test AsyncAwait

---
+++ AsyncAwait/Coche.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Coche
+    {
+        public bool finDestino { get; set; }
+
+        public async void Conducir()
+        {
+            finDestino = false;
+            Console.WriteLine("Empezando a conducir");
+
+            await TiempoOperacion();
+
+            Console.WriteLine("\nA llegado a su destino");
+
+            finDestino = true;
+        }
+
+        private Task TiempoOperacion()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Conduciendo");
+                Thread.Sleep(3000);
+            });
+        }
+    }
+}
---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando ruta");
+
+            var coche = new Coche();
+            coche.Conducir();
+
+            while (!coche.finDestino)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Finalizando ruta");
+            Console.ReadKey();
+        }
+    }
+}
